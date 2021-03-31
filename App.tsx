efe5c342739b16159970cae703c72afc47429600
Brent Vatne <brentvatne@gmail.com>
2021-03-31 13:01:47 -0700

    repro

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LottieView source={require('./animation.json')} autoPlay loop />
       <StatusBar style="auto" />
     </View>
   );
